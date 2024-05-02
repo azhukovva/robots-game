7130a85de977ff1778b8ae768954ca68b38d014b
switos <atomol112@gmail.com>
2024-05-02 21:28:38 +0200

    fxml insertion

--- src/main/java/vut/ija2023/HelloApplication.java
+++ src/main/java/vut/ija2023/HelloApplication.java
@@ -3,24 +3,21 @@ package vut.ija2023;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Hello");
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setTitle("Hellow World");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+    public static void main(String[] args) {
+        launch();
     }
 }
